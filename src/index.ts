5903f7bf2495120c14780f8185a07534f896d3da
PerQuis! <thetahirpl@gmail.com>
2022-07-04 21:30:59 +0200

    A small express application was created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('test');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+const app = express();
+
+dotenv.config({ path: '.env' });
+
+mongoose
+   .connect(process.env.MONGO_DB_URI)
+   .then(() => console.log('Mongo connected.'))
+   .catch((err) => console.error(err.message));
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}.`));
