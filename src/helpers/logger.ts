c84336448ed7c2d747627dea0ba25ea67bee1524
PerQuis! <thetahirpl@gmail.com>
2022-07-04 22:17:18 +0200

    Improved key of dirname for transports in logger.ts file

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,11 +1,15 @@
 import winston from 'winston';
+import path from 'path';
+
+const dirname = path.resolve(__dirname, '../log');
 
 const logger = winston.createLogger({
    level: 'info',
    format: winston.format.json(),
    transports: [
-      new winston.transports.File({ dirname: './src/log', filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ dirname: './src/log', filename: 'logFile.log' }),
+      new winston.transports.File({ dirname, filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ dirname, filename: 'warn.log', level: 'warn' }),
+      new winston.transports.File({ dirname, filename: 'info.log' }),
    ],
 });
 
